4b3d1f3d45600599c010b9e4d937a12579b4a249
JinSan-RM <yug6789@naver.com>
2024-08-30 17:55:18 +0900

    [prompt] prompt tree 아키텍쳐 구조 짜임새 있게 하기

--- PDF_base_AI/PDF/PDF_base_CLOVA.py
+++ PDF_base_AI/PDF/PDF_base_CLOVA.py
@@ -4,6 +4,48 @@ import requests
 
 NCP_API_KEY = os.environ.get('NCP_API_KEY')
 NCP_API_KEY_primary_val = os.environ.get('NCP_API_KEY_primary_val')
+tree_structure = """- 트리구조는 이렇게 제시하면 돼. C:.
+                                                    ├─메인
+                                                    ├─회사소개
+                                                    │  ├─CEO 인사말
+                                                    │  ├─조직도
+                                                    │  └─오시는 길
+                                                    │
+                                                    ├─사업소개
+                                                    │   ├─사업분야
+                                                    │   ├─사업성과
+                                                    │   └─제품소개
+                                                    │
+                                                    └─고객센터
+                                                        ├─공지사항
+                                                        ├─고객문의
+                                                        └─FAQ"""
+
+content_structure = """ 1. 메인
+                        내용 : 회사의 비전과 미션을 간략히 소개.
+                        2. 회사 소개 (About Us)
+                            2-1. CEO 인사말 
+                            내용 : 회사의 기본 정보, 목표, 비전.
+                            2-2. 조직도
+                            내용 : 회사의 조직 구조 및 주요 인물 소개.
+                            2-3. 연혁
+                            내용 : 연도에 따른 회사 성장 과정.
+                        3. 사업 소개 (Services)
+                            3-1. 사업분야
+                            내용 : 회사의 사업 아이템, 사업 분야.
+                            3-2. 사업성과
+                            내용 : 회사의 사업 성과
+                            3-3. 제품소개
+                            내용 : 회사 제품 및 서비스에 대한 설명
+                        4. 고객센터
+                            4-1. 공지사항
+                            내용 : 회사의 최신 소식 및 이벤트.
+                            4-2. 고객문의
+                            내용 : 연락처 정보, 이메일, 소셜 미디어 링크 등.
+                            4-3. FAQ
+                            내용 : 자주 묻고 답하는 질문 및 답변.
+                         """
+
 
 class CompletionClova:
     def __init__(self, host, api_key, api_key_primary_val, request_id):
@@ -21,11 +63,11 @@ class CompletionClova:
             'Accept': 'text/event-stream'
         }
 
-        with requests.post(self._host + '/testapp/v1/chat-completions/HCX-003',
-                           headers=headers, json=completion_request, stream=False) as r:
-            for line in r.iter_lines():
-                if line:
-                    print(line.decode("utf-8"))
+        response = requests.post(self._host + '/testapp/v1/chat-completions/HCX-003',
+                                headers=headers, json=completion_request, stream=False)
+        response_text = response.text
+        print(response_text)
+        return response_text  # 이 부분에서 response_text를 반환
 
 def PDF_Menu_Create_C(text):
         completion_executor = CompletionClova(
@@ -36,7 +78,7 @@ def PDF_Menu_Create_C(text):
         )
         preset_text = [{"role":"system","content":"- 너는 기업소개 웹 사이트 기획자야. \n- 데이터는 회사소개서 혹은 제품소개서야. \n- 결과값은 2가지로 출력할거야. 하나는 이중 구조로 10개의 서브메뉴를 트리구조로 그리는 것. 다른 하나는 각 메뉴에 들어갈 내용을 적어줘"},
                    {"role":"user","content": text},
-                   {"role":"assistant","content":"소셜 미디어는 사람들의 도파민을 풍성하게 해줘"}]
+                   {"role":"assistant","content":f"{tree_structure}, {content_structure}"}]
 
         request_data = {
             'messages': preset_text,
@@ -51,4 +93,3 @@ def PDF_Menu_Create_C(text):
         }
         
         return completion_executor.execute(request_data)
-    
--- PDF_base_AI/PDF/PDF_base_GPT.py
+++ PDF_base_AI/PDF/PDF_base_GPT.py
@@ -6,35 +6,72 @@ OpenAI.api_key = os.environ.get('OPENAI_API_KEY')
 
 client = OpenAI()
 
-# class CompletionGPT:
-#     def __init__(self, host, text):
-#         self._host = host
-#         self._text = text
+
+
+tree_structure = """- You can use this kind of structure when build a tree structure.
+                    C:.
+                    ├─Main
+                    ├─About Us
+                    │  ├─CEO's Message
+                    │  ├─Organization Chart
+                    │  └─Directions
+                    │
+                    ├─Business Overview
+                    │   ├─Business Areas
+                    │   ├─Business Achievements
+                    │   └─Product Introduction
+                    │
+                    └─Customer Service
+                        ├─Notices
+                        ├─Customer Inquiries
+                        └─FAQ"""
+
+content_structure = """ - You can use this when make contents of menues
+                    1. Main
+                    Content: A brief introduction to the company's vision and mission.
+
+                    2. About Us
+                        2-1. CEO's Message
+                            Content: Basic company information, goals, and vision.
+                        2-2. Organization Chart
+                            Content: Introduction to the company's organizational structure and key personnel.
+                        2-3. History
+                            Content: The company's growth process over the years.
+                    
+                    3. Services
+                        3-1. Business Areas
+                            Content: The company's business items and areas of operation.
+                        3-2. Business Achievements
+                            Content: The company's business accomplishments.
+                        3-3. Product Introduction
+                            Content: Description of the company's products and services.
+                    
+                    4. Customer Service
+                        4-1. Notices
+                            Content: The company's latest news and events.
+                        4-2. Customer Inquiries
+                            Content: Contact information, email, social media links, etc.
+                        4-3. FAQ
+                            Content: Frequently asked questions and answers. """
+
+class CompletionGPT:
+    def __init__(self, host, text):
+        self._host = host
+        self._text = text
 
 def PDF_Menu_Create_G(text):
     truncated_text = truncate_text_to_token_limit(text)
     completion = client.chat.completions.create(
         model="gpt-4",
         messages=[
-            {
-                "role": "system",
-                "content": "You are the organizer of the company introduction website."
-            },
-            {
-                "role": "user",
-                "content": truncated_text
-            },
-            {
-                "role": "assistant",
-                "content": "Understood. I will help you create a structured web header menu based on the contents you provide. I will ensure that the submenus are logically related to the main menus, and the entire structure will be clear and organized, with a maximum of 10 items in a tree format."
-            },
-            {
-                "role": "system",
-                "content": "You will perform two tasks: 1) Extract the main keywords from the text. 2) Write a brief introduction based on the text."
-            }
-            # {"role": "system", "content": "- You are the organizer of the company introduction website. \n- Data is a company introduction letter or product introduction letter. \n- The result values will be printed in two ways. One is to draw 10 submenus in a tree structure. The other is to write down what goes into each menu"},
-            # {"role": "user", "content": truncated_text},
-            # {"role": "assistant", "content": "Understood. I will help you create a structured web header menu based on the contents you provide. I will ensure that the submenus are logically related to the main menus, and the entire structure will be clear and organized, with a maximum of 10 items in a tree format."},
+            {"role": "system", "content": "- You are the organizer of the company introduction website. \n- Data is a company introduction letter or product introduction letter. \n- The result values will be printed in two ways. One is to draw 10 submenus in a tree structure. The other is to write down what goes into each menu"},
+            {"role": "user", "content": truncated_text},
+            {"role": "assistant", "content": f"{tree_structure}, {content_structure}"}
+            # ,{
+            # "role": "system",
+            # "content": "You will perform two tasks: 1) Extract the main keywords from the text. 2) Write a brief introduction based on the text."
+            # },
+            # {"role": "user", "content": }
         ],
         temperature=0.1,
         max_tokens=300,
